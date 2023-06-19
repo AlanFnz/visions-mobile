d4a0b4f7eeb7238d2d881afdb3d931e5434910ae
Alan Fernandez <alan.f@msn.com>
2023-06-19 18:51:37 +0100

    wip: updated unit test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,43 @@
 /**
- * @format
+ * FIXME: create separate config for e2e and unit tests
  */
-
-import 'react-native';
 import React from 'react';
+import { render } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import App from '../src/App';
+import { NavigationContainer } from '@react-navigation/native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+jest.mock('../src/feature/Signup', () => () => null);
+jest.mock('../src/feature/Login', () => () => null);
+jest.mock('../src/services/firebase', () => ({
+  firebaseCreateWithEmailAndPassword: jest.fn(),
+  firebaseSignInWithEmailAndPassword: jest.fn()
+}));
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    NavigationContainer: ({ children }: any) => children
+  };
+});
+
+const mockStore = configureStore([]);
+
+describe('App', () => {
+  it('should render the login screen when not logged in', () => {
+    const store = mockStore({
+      auth: { isLoggedIn: false }
+    });
+
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <NavigationContainer>
+          <App />
+        </NavigationContainer>
+      </Provider>
+    );
+
+    expect(getByTestId('loginScreen')).toBeDefined();
+  });
 });
