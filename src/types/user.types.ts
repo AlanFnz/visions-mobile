4849c7e0d37bd11cfddf3674a9bb2d766a857d4b
Alan Fernandez <alan.f@msn.com>
2023-06-24 21:20:12 +0100

    user types

---
+++ src/types/user.types.ts
@@ -0,0 +1,19 @@
+export type User = {
+  uid?: string | null;
+  email?: string | null;
+};
+
+export type UserData = User & {
+  firstName?: string;
+  lastName?: string;
+  firstLast?: string;
+  profilePicture?: string;
+  userId?: string;
+  signUpDate?: string;
+};
+
+export type AuthState = {
+  token: string | null;
+  userData: UserData | null;
+  didTryAutoLogin: boolean;
+};
